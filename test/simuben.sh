df3a29e58be901a95fac9a1d04000809fa2138b9
vityaman <vityaman.dev@yandex.ru>
2025-12-11 05:24:16 +0000

    Guide

--- test/simuben.sh
+++ test/simuben.sh
@@ -3,4 +3,8 @@ cat > /tmp/simuben.yml <<TEST
 nexus_am:
   path: $HOME/nexus-am
   toolchain_path: $(llvm-config --prefix)
+verilator:
+  path: $HOME/emu
+nemu:
+  path: $HOME/NEMU/build/riscv64-nemu-interpreter
 TEST
